a6358026213fb6a98e8183aa606c168183a11a16
AndrewVSS <85450088+AndrewVSS@users.noreply.github.com>
2023-01-23 18:52:52 +0300

    first upload

---
+++ src/App.vue
@@ -0,0 +1,26 @@
+<script setup>
+
+</script>
+
+<template>
+  <div>
+    <header>
+      <div>
+        <img class="img" src="http://promalpufa.ru/wp-content/themes/promalp/img/header-bg.webp">
+      </div>
+
+    </header>
+
+  </div>
+
+</template>
+
+
+<style lang="less" scoped>
+.img {
+  width: 100%;
+  object-fit: cover;
+}
+</style>
+
+
